88630c6a1945712ea277732fb1c99f6ea04f8114
vivek <chimnanivivek14@gmail.com>
2024-01-11 12:42:58 +0530

    fixed some styling issues in login form

--- client/src/components/login/LoginFooter.jsx
+++ client/src/components/login/LoginFooter.jsx
@@ -4,22 +4,6 @@ import { Link } from "react-router-dom";
 const LoginFooter = ({ classes }) => {
   return (
     <footer className={classes.login_footer}>
-      <div className={classes.login_footer_wrap}>
-        <Link>English(UK)</Link>
-        <Link>Français(FR)</Link>
-        <Link>العربية</Link>
-        <Link>ⵜⴰⵎⴰⵣⵉⵖⵜ</Link>
-        <Link>Español (España)</Link>
-        <Link>italiano</Link>
-        <Link>Deutsch</Link>
-        <Link>Português (Brasil)</Link>
-        <Link>हिन्दी</Link>
-        <Link>中文(简体)</Link>
-        <Link>日本語</Link>
-        <Link className={classes.footer_square}>
-          <i className={classes.plus_icon}></i>
-        </Link>
-      </div>
       <div className={classes.footer_splitter}></div>
       <div className={classes.login_footer_wrap}>
         <Link>Sign Up</Link>
